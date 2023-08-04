59f4a6d5aeff7cf966e7e087f64e6c704715474e
Daniel Reynoso Sanchez <dreynoso@tribalworldwide.gt>
2023-08-04 08:54:04 -0600

    created multiblocprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:stripe_app/pages/complete_payment.dart';
 import 'package:stripe_app/pages/home_page.dart';
+
+import 'bloc/pagar/pagar_bloc.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,16 +14,22 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Stripe app',
-      routes: {
-        HomePage.routeName: (context) => const HomePage(),
-        CompletePaymentPage.routeName: (context) => const CompletePaymentPage(),
-      },
-      initialRoute: HomePage.routeName,
-      theme: ThemeData.light().copyWith(
-        primaryColor: const Color(0xff284879),
-        scaffoldBackgroundColor: const Color(0xff21232A),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => PagarBloc()),
+      ],
+      child: MaterialApp(
+        title: 'Stripe app',
+        routes: {
+          HomePage.routeName: (context) => const HomePage(),
+          CompletePaymentPage.routeName: (context) =>
+              const CompletePaymentPage(),
+        },
+        initialRoute: HomePage.routeName,
+        theme: ThemeData.light().copyWith(
+          primaryColor: const Color(0xff284879),
+          scaffoldBackgroundColor: const Color(0xff21232A),
+        ),
       ),
     );
   }
